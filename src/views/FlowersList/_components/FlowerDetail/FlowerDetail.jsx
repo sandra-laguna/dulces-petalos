67de395d6942da4e40aee8b05803313665cfd670
sandra-laguna <idsandrax@gmail.com>
2022-04-08 01:14:23 +0200

    add spinner in FlowerDetail

--- src/views/FlowersList/_components/FlowerDetail/FlowerDetail.jsx
+++ src/views/FlowersList/_components/FlowerDetail/FlowerDetail.jsx
@@ -6,9 +6,11 @@ import { FlowersService } from "services/FlowersService";
 import styles from "./FlowerDetail.module.scss";
 
 import { Button } from "../Button";
+import { Spinner } from "../../../_components/Spinner";
 
 export const FlowerDetail = () => {
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
   const [flowers, setFlowers] = useState([]);
 
   useEffect(() => {
@@ -17,30 +19,39 @@ export const FlowerDetail = () => {
 
   const getInfo = async () => {
     try {
+      setIsLoading(true);
       const flowerData = await FlowersService.getProduct(id);
       setFlowers(flowerData);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <section className={styles.flowerDetail}>
-      <aside>
-        <img src={flowers?.imgUrl} alt={flowers?.name} title={flowers?.name} />
-      </aside>
-      <article>
-        <NavLink to="/">
-          <Button text="Atrás" />
-        </NavLink>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <aside>
+            <img src={flowers?.imgUrl} alt={flowers?.name} title={flowers?.name} />
+          </aside>
+          <article>
+            <NavLink to="/">
+              <Button text="Atrás" />
+            </NavLink>
 
-        <h2>{flowers?.name}</h2>
-        <p>{flowers?.binomialName}</p>
-        <p>{flowers?.fertilizerType}</p>
-        <p>{flowers?.heightInCm}</p>
-        <p>{flowers?.price}</p>
-        <p>{flowers?.wateringsPerWeek}</p>
-      </article>
+            <h2>{flowers?.name}</h2>
+            <p>{flowers?.binomialName}</p>
+            <p>{flowers?.fertilizerType}</p>
+            <p>{flowers?.heightInCm}</p>
+            <p>{flowers?.price}</p>
+            <p>{flowers?.wateringsPerWeek}</p>
+          </article>
+        </>
+      )}
     </section>
   );
 };
